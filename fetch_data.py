029f19b9489864b607b20d131582b67754f08860
georgiygusev <georgiy.gus@yandex.ru>
2025-07-23 12:16:11 +0300

    fix data saving to minio

--- fetch_data.py
+++ fetch_data.py
@@ -1,32 +1,41 @@
+import asyncio
+import logging
 from datetime import datetime, timedelta
 
 import aiohttp
 import json
 
 
-# <ts> это дата когда выполняется функция, найди от нее вчерашнюю дату и используй
 async def fetch_data(api_token: str, ts: str) -> str:
     url = "https://statistics-api.wildberries.ru/api/v1/supplier/incomes"
     headers = {"Authorization": api_token}
-
     all_incomes = []
 
     dt_ts = datetime.fromisoformat(ts)
-    yesterday = (dt_ts - timedelta(days=1)).strftime("%Y-%m-%d")
+    datefrom = (dt_ts - timedelta(days=1)).strftime("%Y-%m-%d")
 
-    datefrom = yesterday
-
-    async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession(headers=headers) as session:
         while True:
             params = {"dateFrom": datefrom}
-            async with session.get(url, headers=headers, params=params) as response:
-                response.raise_for_status()
-                res = await response.json()
+            res = await fetch_page_with_retry(session, url, params)
 
-                if not res:
-                    break
+            if not res:
+                break
 
-                all_incomes.extend(res)
-                datefrom = res[-1]["lastChangeDate"]
+            all_incomes.extend(res)
+            datefrom = res[-1]["lastChangeDate"]
 
     return json.dumps(all_incomes, indent=2, ensure_ascii=False)
+
+
+async def fetch_page_with_retry(session, url, params):
+    while True:
+        async with session.get(url, params=params) as response:
+            if response.status == 429:
+                retry_after = int(response.headers.get('X-Ratelimit-Retry', 10))
+                logging.warning(f"Rate limited (429). Retrying after {retry_after} seconds...")
+                await asyncio.sleep(retry_after)
+                continue
+
+            response.raise_for_status()
+            return await response.json()
